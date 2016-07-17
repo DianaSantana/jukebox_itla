db7d589c60c79c593849bd1e71b18f24559e9d94
DianaSantana <20142350@itla.edu.do>
2016-07-16 20:41:01 -0500

    Implement Methods

---
+++ src/Vellonera.cpp
@@ -0,0 +1,15 @@
+include "Vellonera.h"
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+#include <cassert>
+#include <cstddef>
+#include <stdexcept>
+#include <iostream>
+#include <memory>
+#include <cstdlib>
+
+//TODO: Declare the methods to use in the main
+int main(){
+	//TODO: implements the main
+}
